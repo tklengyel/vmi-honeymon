f387603eb5f14d4cfeace0ba31241a6526b9e1af
Tamas K Lengyel <tamas.lengyel@zentific.com>
2013-12-17 06:49:25 -0500

    Symbol extractor

---
+++ tools/windows_sym_finder/downloadPDB.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python
+
+''' 
+Download debugging symbols from the Microsoft Symbol Server. 
+Can use as an input an executable file OR a GUID+Age and filename.
+Examples:
+
+$ python symcheck.py -e ntoskrnl.exe
+
+$ python symchk.py -g 32962337f0f646388b39535cd8dd70e82 -s ntoskrnl.pdb
+The GUID+Age here corresponds to the kernel version of the xp-laptop-2005-* images
+The Age value is 0x2.
+
+
+Module Dependencies:
+This script requires the following modules:
+pefile - http://code.google.com/p/pefile/
+construct - http://construct.wikispaces.com/
+To decompress downloaded files you should also have cabextract on your system.
+http://www.cabextract.org.uk/
+
+License: 
+GPL version 3
+http://www.gnu.org/licenses/gpl.html
+
+Miscellaneous References:
+You can see an explanation of the URL format at:
+http://jimmers.info/pdb.html
+'''
+
+import urllib2
+import sys,os
+import os.path
+import pdbparse
+from pefile import PE
+from shutil import copyfileobj
+from urllib import FancyURLopener
+from pdbparse.peinfo import *
+from itertools import islice, count
+
+#SYM_URL = 'http://symbols.mozilla.org/firefox'
+SYM_URLS = ['http://msdl.microsoft.com/download/symbols']
+USER_AGENT = "Microsoft-Symbol-Server/6.6.0007.5"
+
+class PDBOpener(FancyURLopener):
+    version = USER_AGENT
+    def http_error_default(self, url, fp, errcode, errmsg, headers):
+        if errcode == 404:
+            raise urllib2.HTTPError(url, errcode, errmsg, headers, fp)
+        else:
+            FancyURLopener.http_error_default(url, fp, errcode, errmsg, headers)
+
+lastprog = None
+def progress(blocks,blocksz,totalsz):
+    global lastprog
+    if lastprog is None:
+        print "Connected. Downloading data..."
+    percent = int((100*(blocks*blocksz)/float(totalsz)))
+    if lastprog != percent and percent % 5 == 0: print "%d%%" % percent,
+    lastprog = percent
+    sys.stdout.flush()
+
+def download_file(guid,fname,path="",verbose=False):
+    ''' 
+    Download the symbols specified by guid and filename. Note that 'guid'
+    must be the GUID from the executable with the dashes removed *AND* the
+    Age field appended. The resulting file will be saved to the path argument,
+    which default to the current directory.
+    '''
+    
+    # A normal GUID is 32 bytes. With the age field appended
+    # the GUID argument should therefore be longer to be valid.
+    # Exception: old-style PEs without a debug section use 
+    # TimeDateStamp+SizeOfImage
+    if len(guid) == 32:
+        print "Warning: GUID is too short to be valid. Did you append the Age field?"
+
+    for sym_url in SYM_URLS:
+        url = sym_url + "/%s/%s/" % (fname,guid)
+        opener = urllib2.build_opener()
+        
+        # Whatever extension the user has supplied it must be replaced with .pd_
+        tries = [ fname[:-1] + '_', fname ]
+
+        for t in tries:
+            if verbose: print "Trying %s" % (url+t)
+            outfile = os.path.join(path,t)
+            try:
+                hook = None if not verbose else progress
+                PDBOpener().retrieve(url+t, outfile, reporthook=hook)
+                if verbose:
+                    print
+                    print "Saved symbols to %s" % (outfile)
+                return outfile
+            except urllib2.HTTPError, e:
+                if verbose:
+                    print "HTTP error %u" % (e.code)
+    return None
+
+def handle_pe(pe_file):
+    dbgdata, tp = get_pe_debug_data(pe_file)
+    pe_header =  PE(pe_file, fast_load=True)
+    pe_guid = "%.8x%.5x" % (pe_header.FILE_HEADER.TimeDateStamp, pe_header.OPTIONAL_HEADER.SizeOfImage)
+    # print "PE GUID: %s" % pe_guid
+    if tp == "IMAGE_DEBUG_TYPE_CODEVIEW":
+        # XP+
+        if dbgdata[:4] == "RSDS":
+            (guid,filename) = get_rsds(dbgdata)
+        elif dbgdata[:4] == "NB10":
+            (guid,filename) = get_nb10(dbgdata)
+        else:
+            print "ERR: CodeView section not NB10 or RSDS"
+            return
+        saved_file = download_file(guid.upper(),filename)
+        pdb = "%s:%s:%s" % (filename, pe_guid, guid)
+    elif tp == "IMAGE_DEBUG_TYPE_MISC":
+        # Win2k
+        # Get the .dbg file
+        guid = get_pe_guid(pe_file)
+        filename = get_dbg_fname(dbgdata)
+        pdb = "%s:%s:%s" % (filename, pe_guid, guid)
+        saved_file = download_file(guid.upper(),filename)
+
+        # Extract it if it's compressed
+        # Note: requires cabextract!
+        if saved_file.endswith("_"):
+            os.system("cabextract -q %s" % saved_file)
+            saved_file = saved_file.replace('.db_','.dbg')
+
+        from pdbparse.dbgold import DbgFile
+        dbgfile = DbgFile.parse_stream(open(saved_file))
+        cv_entry = [ d for d in dbgfile.IMAGE_DEBUG_DIRECTORY
+                       if d.Type == "IMAGE_DEBUG_TYPE_CODEVIEW"][0]
+        if cv_entry.Data[:4] == "NB09":
+            return
+        elif cv_entry.Data[:4] == "NB10":
+            (guid,filename) = get_nb10(cv_entry.Data)
+            
+            guid = guid.upper()
+            saved_file = download_file(guid,filename)
+        else:
+            print "WARN: DBG file received from symbol server has unknown CodeView section"
+            return
+    else:
+        print "Unknown type:",tp
+        return
+
+    if saved_file.endswith("_"):
+        os.system("cabextract -q %s" % saved_file)
+        os.system("rm %s" % saved_file)
+        os.system("mv %s %s" % (filename, pdb))
+        print "%s" % pdb
+
+def get_pe_from_pe(filename):
+    guid = get_pe_guid(filename)
+    symname = os.path.basename(filename)
+    saved_file = download_file(guid, symname)
+    if saved_file.endswith("_"):
+        os.system("cabextract -q %s" % saved_file)
+
+def main():
+    global SYM_URLS
+
+    if not sys.stdin.isatty():
+        input = sys.stdin.readline()
+        input=input.strip('\r\n')
+        handle_pe(input)
+        
+if __name__ == "__main__":
+    main()
---
+++ tools/windows_sym_finder/symlookup.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+import pdbparse
+import sys, os
+import string
+from operator import itemgetter,attrgetter
+from bisect import bisect_right
+from pdbparse.undecorate import undecorate
+from pdbparse.undname import undname
+from itertools import islice, count
+
+class DummyOmap(object):
+    def remap(self, addr):
+        return addr
+
+class Lookup(object):
+    def __init__(self, pdbname):
+        self.addrs = {}
+        self._cache = {}
+
+        not_found = []
+
+        pdbbase = ".".join(os.path.basename(pdbname).split('.')[:-1])
+        guids = pdbname.partition(":")[2]
+        pe_guid = guids.partition(":")[0]
+        pdb_guid = guids.partition(":")[2]
+
+        if not os.path.exists(pdbname):
+            print "WARN: %s not found" % pdbname
+        try:
+            header = open("%s.h" % pdbbase, 'w')
+
+            header.write("const char *%s_guid[2] = {\n\t\"%s\",\n\t\"%s\"\n\t};\n" %(pdbbase, pe_guid, pdb_guid))
+            header.write("const struct symbol %s[] = {\n" % pdbbase)
+
+            print "Loading symbols for %s..." % pdbbase
+            # Do this the hard way to avoid having to load
+            # the types stream in mammoth PDB files
+            pdb = pdbparse.parse(pdbname, fast_load=True)
+            pdb.STREAM_DBI.load()
+            pdb._update_names()
+            pdb.STREAM_GSYM = pdb.STREAM_GSYM.reload()
+            pdb.STREAM_GSYM.load()
+            pdb.STREAM_SECT_HDR = pdb.STREAM_SECT_HDR.reload()
+            pdb.STREAM_SECT_HDR.load()
+            # These are the dicey ones
+            pdb.STREAM_OMAP_FROM_SRC = pdb.STREAM_OMAP_FROM_SRC.reload()
+            pdb.STREAM_OMAP_FROM_SRC.load()
+            pdb.STREAM_SECT_HDR_ORIG = pdb.STREAM_SECT_HDR_ORIG.reload()
+            pdb.STREAM_SECT_HDR_ORIG.load()
+
+        except AttributeError, e:
+            pass
+
+        try:
+            sects = pdb.STREAM_SECT_HDR_ORIG.sections
+            omap = pdb.STREAM_OMAP_FROM_SRC
+        except AttributeError as e:
+            # In this case there is no OMAP, so we use the given section
+            # headers and use the identity function for omap.remap
+            sects = pdb.STREAM_SECT_HDR.sections
+            omap = DummyOmap()
+
+        gsyms = pdb.STREAM_GSYM
+
+        last_sect = max(sects, key=attrgetter('VirtualAddress'))
+        limit = last_sect.VirtualAddress + last_sect.Misc.VirtualSize
+
+        self.addrs[0,limit] = {}
+        self.addrs[0,limit]['name'] = pdbbase
+        self.addrs[0,limit]['addrs'] = []
+
+        sym_counter = []
+        specials = '@\':`~ <>*,?'+chr(94)
+        trans = string.maketrans(specials, '_'*len(specials))
+
+        counter = 0;
+
+        for sym in gsyms.globals:
+
+            if not hasattr(sym, 'offset'):
+                print "No offset attribute"
+                continue
+
+            off = sym.offset
+            try:
+                virt_base = sects[sym.segment-1].VirtualAddress
+            except IndexError:
+                print "IndexError"
+                continue
+
+            mapped = omap.remap(off+virt_base)
+            sym_name = sym.name
+            if sym_name.startswith("?"):
+                sym_name = undname(sym_name)
+
+            sym_name = sym_name.translate(trans)
+
+            sym_counter.append(sym_name);
+            occurance = sym_counter.count(sym_name);
+
+            if occurance > 1:
+                sym_name += "_%u" % occurance
+
+            if mapped > 0:
+                counter = counter +1
+                header.write("\t{.name = \"%s\", .rva = 0x%lx},\n" % (sym_name, mapped))
+
+        header.write("\n};\n");
+        header.write("const uint64_t %s_count = %u;\n" %(pdbbase, counter))
+        header.close()
+
+if __name__ == "__main__":
+
+    if not sys.stdin.isatty():
+        input = sys.stdin.readline()
+        input=input.strip('\r\n')
+        lobj = Lookup(input)
+    else:
+        print "Input has to be piped into this script"
